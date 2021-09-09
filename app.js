06daa12b6cadf54c8456ae558c70ee328e69147d
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2021-09-09 14:10:51 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,6 @@
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+var outputdiv = document.querySelector("#output");
+btnTranslate.addEventListener("click", function clickHandler(){
+    outputdiv.innerHTML = " sjsjjsjsjsj "+ txtInput.value ;
+})
