8df7fe92ecd36129e70576c49e2f6f23e761f048
Krishanu Dutta <hrickkrishanudutta@gmail.com>
2021-09-12 22:31:09 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputdiv = document.querySelector("#output");
 // var serverURL = "https://lessonfourapi.tanaypratap.repl.co/translate/yoda.json"
-var serverURL = "https://api.funtranslations.com/translate/minion.json"
+// var serverURL = "https://api.funtranslations.com/translate/minion.json"
+var serverURL = "https://api.funtranslations.com/translate/groot.json"
 
 function getTranslationURL(text){
     return serverURL + "?" + "text="+ text
